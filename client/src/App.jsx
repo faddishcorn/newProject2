641b19d135978f92767b9fccb27fc87edde892a2
faddishcorn <juni129461@gmail.com>
2025-04-09 16:47:28 +0900

    chore: analytics setting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,18 @@
 // src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         {/* 추후 여기에 다른 페이지도 추가 */}
       </Routes>
     </BrowserRouter>
+    <Analytics/>
+    </>
   )
 }
