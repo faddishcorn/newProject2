5816447275776fd24fd5154aff9183ac29fa8fba
faddishcorn <juni129461@gmail.com>
2025-04-09 23:29:13 +0900

    feat: mainLayout

---
+++ client/src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
+
+export default function MainLayout() {
+  return (
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <Navbar />
+      <main className="flex-1 container mx-auto px-4 py-6 sm:px-6 lg:px-8">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
