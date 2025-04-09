f4eec5f5fbbe2f8db5dc1a9729cf0655cc7dee91
faddishcorn <juni129461@gmail.com>
2025-04-09 19:54:56 +0900

    feat: Server setting

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ MongoDB connected');
+    app.listen(5000, () => console.log('🚀 Server running on port 5000'));
+  })
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
