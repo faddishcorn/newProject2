99e2e1d5fe9d0c5bc5faf1049c1f44472bedf5f3
faddishcorn <juni129461@gmail.com>
2025-04-13 01:03:48 +0900

    chore: toastify install

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,14 @@ import LandingPage from './pages/LandingPage'
 import { Analytics } from "@vercel/analytics/react"
 import LoginPage from './pages/LoginPage'
 import AppRoutes from './routes'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function App() {
   return (
     <>
     <AppRoutes/>
+    <ToastContainer position="top-center" autoClose={3000} />
     <Analytics/>
     </>
   )
